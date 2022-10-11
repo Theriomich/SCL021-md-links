54b66f091962df6a1a1eb3bb363d05a025264516
Theriomich <theriomich@gmail.com>
2022-10-11 10:08:47 -0300

    Agregue carpeta con links a leer

--- mdLinks.js
+++ mdLinks.js
@@ -1,16 +1,20 @@
+const fs = require("fs")
 
-// 
-const os = require("os")
+const someL = fs.readFile("Files/someLinks.md")
 
-console.log(os.platform)
-console.log(os.release)
-console.log(os.totalmem)
+console.log(someL)
+//
+// const os = require("os")
+
+// console.log(os.platform)
+// console.log(os.release)
+// console.log(os.totalmem)
 // console.log(path)
 
 // console.log(edad)
 // console.log(gatito)
-console.table({
-    os: os.platform(),
-    version: os.release(),
-    gatitoMem: os.totalmem(),
-}) 
+// console.table({
+//     os: os.platform(),
+//     version: os.release(),
+//     gatitoMem: os.totalmem(),
+// }) 
