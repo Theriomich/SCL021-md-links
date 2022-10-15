69a0d4b1a9af4d74279a0cb0e9d6dff7bbd50d1a
Theriomich <theriomich@gmail.com>
2022-10-14 22:10:19 -0300

    Funciona convertir ruta en absoluta y ver si el archivo es md

--- mdLinks.js
+++ mdLinks.js
@@ -1,10 +1,46 @@
 import path from "path"
 import fs from "fs"
 import chalk from "chalk"
-//import answers from "."
+
 
 //Si la ruta no es absoluta convertir a absoluta
 const convertToAbsolutePath = (file) => {
     if (path.isAbsolute(file) === false) { return path.resolve(file) }
+    else { return file }
 }
-console.log(convertToAbsolutePath)
+//Ver si al archivo es de tipo Md
+const searchMd = (file) => {
+    if (path.extname(file) === ".md") { return true }
+    else { return false }
+}
+
+//Si es md comprobar si tiene links
+const fileRead = (pathToRead) => {
+    const file = fs.readFileSync(pathToRead, "uft8")
+    const regExpHttp = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
+    //Si el archivo contiene links convertir sus propiedades en un objeto
+    const parseFile = path.parse(file)
+    if (file.includes(regExpHttp)) { return parseFile }
+
+
+
+    // let resul =
+    //     let link = []
+    // while ((result = regExpHttp.exec(parseFile)) !== null) {
+    //     let obj{
+    //         href: result[0, 3],
+    //         text: result[0, 4],
+    //         file: pathToRead
+    //     }
+    //     link.push(obj)
+    // }
+    // return link
+
+}
+
+console.log(parseFile("Files/lala.md"))
+
+
+
+//console.log(convertToAbsolutePath("Files/someLinks.md"))
+
