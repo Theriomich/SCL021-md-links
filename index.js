d2eeefa30ae912f3a0ff97617b40bba51eb86afd
Theriomich <theriomich@gmail.com>
2022-10-19 20:43:18 -0300

    Funcionando meter los links a un array

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
-import welcome from "./welcome.js"
-import inquirer from "inquirer"
-import path from "path"
-import chalk from "chalk"
-import { verificateFileExist, convertToAbsolutePath, searchMd, fileRead } from "./mdLinks.js"
+import welcome from "./welcome.js";
+import inquirer from "inquirer";
+import path from "path";
+import chalk from "chalk";
+import {
+  verificateFileExist,
+  convertToAbsolutePath,
+  searchMd,
+  linkRead,
+} from "./mdLinks.js";
 
-
-const yellow = chalk.bold.yellow
-console.log(welcome)
+console.log(welcome);
 
 setTimeout(() => {
-    inquirer.prompt({
+  inquirer
+    .prompt({
+      name: "archivo",
+      message: "Ingrese la ruta del archivo",
+      default: "C:/ Users /  carpeta / doc.md",
+    })
+    .then((answers) => {
+      if (verificateFileExist(answers.archivo) === true) {
+        console.log("Answer:", convertToAbsolutePath(answers.archivo));
+        const pathAbsolute = convertToAbsolutePath(answers.archivo);
+        console.log(linkRead(pathAbsolute));
+      } else {
+        console.log(chalk.redBright("Debe ingresar una ruta valida"));
+      }
 
-        name: "archivo",
-        message: "Ingrese la ruta del archivo",
-        default: "C:/ Users /  carpeta / doc.md"
-
-
-    }).then((answers => {
-        if (verificateFileExist(answers.archivo) === true) {
-            console.log("Answer:", convertToAbsolutePath(answers.archivo))
-        }
-
-        else { console.log(chalk.redBright("Debe ingresar una ruta valida")) }
-
-        if (searchMd(answers.archivo) === false && verificateFileExist(answers.archivo) === true) { console.log(chalk.yellowBright("Archivos MD no encontrados")) }
-
-
-
-    }))
-
-
-}, 3000)
-
-
-
-
-
-
-
-
+      if (
+        searchMd(answers.archivo) === false &&
+        verificateFileExist(answers.archivo) === true
+      ) {
+        console.log(chalk.yellowBright("Archivos MD no encontrados"));
+      }
+    });
+}, 3000);
--- mdLinks.js
+++ mdLinks.js
@@ -1,68 +1,87 @@
-import path from "path"
-import fs from "fs"
-import chalk from "chalk"
+import path from "path";
+import fs from "fs";
+import chalk from "chalk";
+import { url } from "inspector";
+import https from "https";
 //import fs from "fs"
 
 //Comprobar si existe un archivo
 const verificateFileExist = (file) => {
-    if (fs.existsSync(file)) { return fs.existsSync(file) }
-
-}
+  if (fs.existsSync(file)) {
+    return fs.existsSync(file);
+  }
+};
 
 //Si la ruta no es absoluta convertir a absoluta
 const convertToAbsolutePath = (file) => {
-    if (path.isAbsolute(file) === false) { return path.resolve(file) }
-    else { return file }
-}
+  if (path.isAbsolute(file) === false) {
+    return path.resolve(file);
+  } else {
+    return file;
+  }
+};
 //Ver si al archivo es de tipo Md
 const searchMd = (file) => {
-    if (path.extname(file) === ".md") { return true }
-    else { return false }
-}
+  if (path.extname(file) === ".md") {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 //Si es md comprobar si tiene links
-const fileRead = (pathToRead) => {
-    const file = fs.readFileSync(pathToRead, "uft8")
-    const regExpHttp = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
-    //Si el archivo contiene links convertir sus propiedades en un objeto
-    const parseFile = path.parse(pathToRead)
-    console.log(pathToRead.match(regExpHttp))
+const linkRead = (pathToRead) => {
+  const file = fs.readFileSync(pathToRead, "utf-8");
+  const newLinks = [];
+  const regExpHttp =
+    /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/gim;
+  //Si el archivo contiene links meterlos en un arreglo
+  const parseFile = path.parse(pathToRead);
 
-    if (pathToRead.match(regExpHttp)) { return parseFile }
+  if (file.match(regExpHttp) === null) {
+    return false;
+  } else if (file) {
+    file.match(regExpHttp).forEach((links) => {
+      newLinks.push(links);
+    });
+  }
+  return newLinks;
+};
 
-    console.log(parseFile("Files/someLinks.md"))
+// function replaceURLs(message) {
+//     if (!message) return;
 
+//     var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+//     return message.replace(urlRegex, function (url) {
+//         var hyperlink = url;
+//         if (!hyperlink.match('^https?:\/\/')) {
+//             hyperlink = 'http://' + hyperlink;
+//         }
+//         return '<a href="' + hyperlink + '" target="_blank" rel="noopener noreferrer">' + url + '</a>'
+//     });
+// }
 
-
-    // function detectURLs(message) {
-    //     var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
-    //     return message.match(urlRegex)
-
-    // }
-    // console.log(message("Files/someLinks.md"))
-
-    // let resul =
-    //     let link = []
-    // while ((result = regExpHttp.exec(parseFile)) !== null) {
-    //     let obj{
-    //         href: result[0, 3],
-    //         text: result[0, 4],
-    //         file: pathToRead
-    //     }
-    //     link.push(obj)
-    // }
-    // return link
-
+function detectURLs(message) {
+  let urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+  fs.readFileSync(message);
+  return message.match(urlRegex);
 }
 
+//console.log(detectURLs(`Files/someLinks.md`));
 
-export { verificateFileExist, convertToAbsolutePath, searchMd, fileRead }
+// }
+//console.log(print(urls))
 
+// let resul =
+//     let link = []
+// while ((result = regExpHttp.exec(parseFile)) !== null) {
+//     let obj{
+//         href: result[0, 3],
+//         text: result[0, 4],
+//         file: pathToRead
+//     }
+//     link.push(obj)
+// }
+// return link
 
-
-
-
-
-
-//console.log(convertToAbsolutePath("Files/someLinks.md"))
-
+export { verificateFileExist, convertToAbsolutePath, searchMd, linkRead };
